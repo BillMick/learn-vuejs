f0b4d4c08e8a6fa3dd94194e142abbbf748c494d
AHOUANDJINOU Bill <mikeahoudjinou@gmail.com>
2024-08-25 16:15:54 +0200

    Counter demo.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
-  <h1>
-    Hey hey {{ firstname.toUpperCase() }}.
-  </h1>
-  <p>I am back here.</p>
+  <p :style="{color: count > 5 ? 'red': 'green'}">My counter: {{ count }} .</p>
+  <button @click="increment">Increment counter.</button>
+  <button @click="decrement">Decrement counter.</button>
+  <div v-if="alert">Congrats !! You have clicked more than five times.</div>
 </template>
 
 <script setup>
-const firstname = 'John'
+  import {ref} from 'vue'
+  const count = ref(0);
+  const alert = ref(false);
+  const number_of_click = ref(0);
+  const increment = function () {
+    count.value++;
+    number_of_click.value++;
+    if (number_of_click.value > 5) {
+      alert.value = true;
+    }
+  }
+  const decrement = function () {
+    count.value--;
+    number_of_click.value++;
+    if (number_of_click.value > 5) {
+      alert.value = true;
+    }
+  }
+  
+  // setInterval(() => {
+  //   count.value++
+  // }, 1000);
+
 </script>
+
+<style>
+  div{
+    color: olive;
+  }
+</style>
